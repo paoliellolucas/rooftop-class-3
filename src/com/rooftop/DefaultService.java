871fb601661b25d24384fc8f962d3d39c2483b4d
lucas.paoliello <FWAR00036@fulcrumdigital.com>
2022-04-04 20:46:55 -0300

    My new feature

--- src/com/rooftop/DefaultService.java
+++ src/com/rooftop/DefaultService.java
@@ -5,5 +5,13 @@ public class DefaultService implements Service {
     @Override
     public void run() {
         System.out.println("Hi!");
+        System.out.println("Hi!");
+        System.out.println("Hi!");
+        System.out.println("Hi!");
+    }
+
+    @Override
+    public void runTwo() {
+        System.out.println("hI!");
     }
 }
--- src/com/rooftop/Service.java
+++ src/com/rooftop/Service.java
@@ -2,4 +2,5 @@ package com.rooftop;
 
 public interface Service {
     void run();
+    void runTwo();
 }
